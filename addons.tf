96abbd991a4fc6f3b848eb200dcc2aac88b4ee13
Fabien Sanglier <Fabien.Sanglier@ibm.com>
2025-06-26 22:54:24 -0700

    update with argocd addon

--- addons.tf
+++ addons.tf
@@ -163,3 +163,33 @@ resource "kubernetes_deployment" "aws_asg_roller" {
   }
 }
 
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  version    = "8.1.1"
+  timeout    = "1500"
+  namespace  = kubernetes_namespace.argocd.id
+  values = [
+    file("${path.module}/values/argocd.yaml")
+}
+
+resource "null_resource" "password" {
+  provisioner "local-exec" {
+    working_dir = "/tmp/argocd"
+    command     = "kubectl -n argocd-staging get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
+  }
+}
+
+resource "null_resource" "del-argo-pass" {
+  depends_on = [null_resource.password]
+  provisioner "local-exec" {
+    command = "kubectl -n argocd-staging delete secret argocd-initial-admin-secret"
+  }
+}
